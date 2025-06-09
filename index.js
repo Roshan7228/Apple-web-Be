4515cac34a270ce4c5aac8da97fd279ea1adca6e
Roshan7228 <7930roshankumar@gmail.com>
2025-06-09 18:15:57 +0530

    back-end

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://zesty-caramel-0eaeeb.netlify.app/"],
     credentials: true
 }));
 app.use(express.static("./UploadImage"));
